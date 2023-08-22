df3f757398164b034d60280e85b6359041156ff0
Bongani Baloyi <bonganibaloyi94@gmail.com>
2023-08-22 21:54:55 +0000

    node js basic

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,15 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
 module.exports = displayMessage;
-
-function displayMessage(msg){
-	console.log(msg);
-};
-
